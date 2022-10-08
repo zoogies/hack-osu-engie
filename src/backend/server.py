6b4aa91e0a397015897e166b56635cacd7773149
Ryan Zmuda <ryanzmuda@gmail.com>
2022-10-08 19:28:57 -0400

    :skull: lil bro think he knows docker

--- src/backend/server.py
+++ src/backend/server.py
@@ -1,6 +1,6 @@
 # imports
 import csv
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 from flask_cors import CORS
 from datetime import datetime, timedelta
 
@@ -148,6 +148,16 @@ def api(building=None,stamp="None",range=None):
 
         data = getall(stamp,int(range),building,type) # call the getall method to get data for every single day for this building in this range from a starting date
         return jsonify(data) # return this data as a json
+
+# Path for our main Svelte page
+@app.route("/")
+def base():
+    return send_from_directory('client/public', 'index.html')
+
+# Path for all the static files (compiled JS/CSS, etc.)
+@app.route("/<path:path>")
+def home(path):
+    return send_from_directory('client/public', path)
 
 # run the server on port 5000 locally
 if __name__ == '__main__':
